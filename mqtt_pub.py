0d183871c72d7ece43b5af948d6b4433b112a8fa
mohammaddawoodshaik <madawood@cisco.com>
2017-01-25 21:19:48 +0530

    Simplified the pub/sub scripts

--- mqtt_pub.py
+++ mqtt_pub.py
@@ -1,115 +1,48 @@
 __author__ = "madawood"
 
-#!/usr/bin/python
+# !/usr/bin/python
 # mqtt demo on Raspberry Pi 3
 # Author madawood using Open Source components
 # Version 1.0  --- January 12, 2017
 # This is the random message publisher thread -- mqtt topics: wx, geo, buttons
 
 import time
-import threading
-import signal
-import random
-import logging
 import random
 import paho.mqtt.client as mqtt
-from subprocess import *
 
-logger = logging.getLogger("mqtt_publish")
-
-wx = [0,0,0]
-geo = [0,0]
+wx = [0, 0, 0]
+geo = [0, 0]
 key = [0]
-def _sleep_handler(signum, frame):
-    print "SIGINT Received. Stopping app"
-    raise KeyboardInterrupt
 
 
-def _stop_handler(signum, frame):
-    print "SIGTERM Received. Stopping app"
-    raise KeyboardInterrupt
-
-
-class MqttPub(threading.Thread):
-    def __init__(self, enabled, host, port, topics, frequency=5):
-        threading.Thread.__init__(self)
-        self.enabled = enabled
-        self.stop_event = threading.Event()
-        self.setDaemon(True)
-        self.host = host
-        self.port = port
-        self.topics = topics
-        self.frequency = frequency
-        logger.debug("Mqtt publisher instance is successfully initialized!")
-
-    def on_connect(self, client, userdata, flags, rc):
-        logger.debug("PUB: Connected with result code :%s" %(str(rc)))
-        client.publish(client._client_id, payload=userdata, qos=0, retain=False)
-        client.disconnect()
-
-    def run(self):
-        if self.enabled:
-            logger.debug("Started the publisher thread!")
-            while True:
-                # Check for mqtt messages to display.
-                if self.stop_event.is_set():
-                    logger.debug("Pub: exiting from thread, as stop event is set!")
-                    self.stop_event.clear()
-                    break
-                for topic in self.topics:
-                    # Create the Publisher connection.
-
-                    if topic == "wx":
-                        wx[0] = random.randrange(240, 310, 1) / 10.0 # Temperature
-                        wx[1] = random.randrange(35, 45, 1) # Humidity
-                        wx[2] = random.randrange(100000, 104000, 1) / 1000.000 # Pressure
-                        logger.debug("PUB: WX is:%s"%wx)
-                        byteArray = bytes(wx)
-                    elif topic == "geo":
-                        geo[0] = random.randrange(129000, 132000, 1) / 10000.0000 # Latitude
-                        geo[1] = random.randrange(770000, 790000, 1) / 10000.0000 # Longitude
-                        logger.debug("PUB: GEO is:%s"%geo)
-                        byteArray = bytes(geo)
-                    elif topic == "buttons":
-                        key[0] = random.choice(["LEFT", "RIGHT", "UP", "DOWN", "SELECT"])
-                        logger.debug("PUB: KEY is:%s"%key)
-                        byteArray = bytes(key)
-                    else:
-                        continue
-                    mqttc = mqtt.Client(client_id=topic, clean_session=True, userdata=byteArray)
-                    print "Connecting to broker : %s:%s" % (self.host, self.port)
-                    mqttc.connect(self.host, port=int(self.port), keepalive=60)
-                    mqttc.on_connect = self.on_connect
-                    mqttc.loop_forever(retry_first_connection=True)
-                    #mqttc.publish(topic, payload=byteArray, qos=0, retain=False)
-                    #mqttc.disconnect()
-                time.sleep(self.frequency)
-        else:
-            logger.info("Pub: Publisher is disabled, so not starting thread!")
-
-    def close(self):
-        logger.debug("pub: stop event is set!")
-        self.stop_event.set()
-
+def run(host, port, topics):
+    print("Started the publisher thread!")
+    while True:
+        for topic in topics:
+            # Create the Publisher connection.
+            if topic == "wx":
+                wx[0] = random.randrange(240, 310, 1) / 10.0  # Temperature
+                wx[1] = random.randrange(35, 45, 1)  # Humidity
+                wx[2] = random.randrange(100000, 104000, 1) / 1000.000  # Pressure
+                print("PUB: WX is:%s" % wx)
+                byteArray = bytes(wx)
+            elif topic == "geo":
+                geo[0] = random.randrange(129000, 132000, 1) / 10000.0000  # Latitude
+                geo[1] = random.randrange(770000, 790000, 1) / 10000.0000  # Longitude
+                print("PUB: GEO is:%s" % geo)
+                byteArray = bytes(geo)
+            elif topic == "buttons":
+                key[0] = random.choice(["LEFT", "RIGHT", "UP", "DOWN", "SELECT"])
+                print("PUB: KEY is:%s" % key)
+                byteArray = bytes(key)
+            else:
+                continue
+            mqttc = mqtt.Client(clean_session=True)
+            print "Connecting to broker : %s:%s" % (host, port)
+            mqttc.connect(host, port=int(port), keepalive=60)
+            mqttc.publish(topic, payload=byteArray)
+            mqttc.disconnect()
+        time.sleep(5)
 
 if __name__ == "__main__":
-    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-    logger.setLevel(10)
-    console = logging.StreamHandler()
-    console.setLevel(10)
-    console.setFormatter(formatter)
-    logger.addHandler(console)
-
-    signal.signal(signal.SIGTERM, _stop_handler)
-    signal.signal(signal.SIGINT, _sleep_handler)
-    m = MqttPub(True, "localhost", 1883, ["wx", "geo", "buttons"])
-    m.start()
-    while True:
-        try:
-            time.sleep(5)
-        except KeyboardInterrupt:
-            m.close()
-            print "App closed"
-            break
-        except Exception as ex:
-            print("Caught exception! Terminating..")
+    run("localhost", 1883, ["wx", "geo", "buttons"])
--- mqtt_sub.py
+++ mqtt_sub.py
@@ -1,112 +1,59 @@
 __author__ = "madawood"
 
-#!/usr/bin/python
+# !/usr/bin/python
 # mqtt demo on Raspberry Pi 3 using Sain Smart or Adafruit 16x2 LCD and button module
 # Author madawood using Open Source components
 # Version 1.3  --- January 11, 2017
 # This is the LCD message subscriber thread.
 
-import time
-import threading
-import signal
 import json
-import logging
 import paho.mqtt.client as mqtt
-from subprocess import *
 
-logger = logging.getLogger("mqtt_app")
 data = {}
 
-def _sleep_handler(signum, frame):
-    print "SIGINT Received. Stopping app"
-    raise KeyboardInterrupt
+
+def on_connect(client, userdata, flags, rc):
+    print("SUB: Connected with result code :%s" % (str(rc)))
+    client.subscribe(str(userdata))
 
 
-def _stop_handler(signum, frame):
-    print "SIGTERM Received. Stopping app"
-    raise KeyboardInterrupt
+def on_message(client, userdata, msg):
+    print("SUB: Payload from : %s" % userdata)
+    print("SUB: Payload is : %s" % msg.payload)
+    payload = msg.payload
+    payload = payload.replace("'", "\"")
+    global data
+    try:
+        payload = json.loads(payload)
+    except ValueError:
+        print("Given payload is not in json format!")
+    if userdata == "wx":
+        data["Temperature"] = payload[0]
+        data["Humidity"] = payload[1]
+        data["Pressure"] = payload[2]
+    elif userdata == "geo":
+        data["Latitude"] = payload[0]
+        data["Longitude"] = payload[1]
+    elif userdata == "buttons":
+        data["Key"] = payload
+    # data = {str(userdata): json.loads(paylaod)}
+    client.disconnect()
 
 
-class MqttSub(threading.Thread):
-    def __init__(self, enabled, host, port, topics):
-        threading.Thread.__init__(self)
-        self.enabled = enabled
-        self.stop_event = threading.Event()
-        self.setDaemon(True)
-        self.host = host
-        self.port = port
-        self.topics = topics
-        logger.debug("Mqtt subscription instance is successfully initialized!")
-
-    def on_connect(self, client, userdata, flags, rc):
-        logger.debug("SUB: Connected with result code :%s" %(str(rc)))
-        client.subscribe(str(userdata))
-
-    def on_message(self, client, userdata, msg):
-        logger.debug("SUB: Payload from : %s" % userdata)
-        logger.debug("SUB: Payload is : %s" % msg.payload)
-        paylaod = msg.payload
-        paylaod = paylaod.replace("'", "\"")
-        global data
-        payload = json.loads(paylaod)
-        if userdata == "wx":
-            data["Temperature"] = payload[0]
-            data["Humidity"] = payload[1]
-            data["Pressure"] = payload[2]
-        elif userdata == "geo":
-            data["Latitude"] = payload[0]
-            data["Longitude"] = payload[1]
-        elif userdata == "buttons":
-            data["Key"] = payload[0]
-        #data = {str(userdata): json.loads(paylaod)}
-        client.disconnect()
-
-
-    def run(self):
-        global data
-        if self.enabled:
-            logger.debug("Started the subscription thread!")
-            while True:
-                if self.stop_event.is_set():
-                    logger.debug("Sub: exiting from thread, as stop event is set!")
-                    self.stop_event.clear()
-                    break
-                try:
-                    for topic in self.topics:
-                        client = mqtt.Client(clean_session=True, userdata=topic)
-                        client.connect(self.host, port=int(self.port), keepalive=60)
-                        client.on_connect = self.on_connect
-                        client.on_message = self.on_message
-                        client.loop_forever(timeout=3)
-                except Exception as ex:
-                    logger.exception("Error in subscription:%s"%str(ex))
-                    break
-        else:
-            logger.info("Sub: subscriber is disabled, so not starting thread!")
-
-    def close(self):
-        logger.debug("Sub: stop event is set!")
-        self.stop_event.set()
+def run(host, port, topics):
+    print("Started the subscription thread!")
+    while True:
+        try:
+            for topic in topics:
+                client = mqtt.Client(clean_session=True, userdata=topic)
+                client.connect(host, port=int(port), keepalive=60)
+                client.on_connect = on_connect
+                client.on_message = on_message
+                client.loop_forever(timeout=3)
+        except Exception as ex:
+            print("Error in subscription:%s" % str(ex))
+            break
 
 
 if __name__ == "__main__":
-    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-    logger.setLevel(10)
-    console = logging.StreamHandler()
-    console.setLevel(10)
-    console.setFormatter(formatter)
-    logger.addHandler(console)
-
-    signal.signal(signal.SIGTERM, _stop_handler)
-    signal.signal(signal.SIGINT, _sleep_handler)
-    m = MqttSub(True, "localhost", 1883, ["wx", "geo", "buttons"])
-    m.start()
-    while True:
-        try:
-            time.sleep(5)
-        except KeyboardInterrupt:
-            m.close()
-            print "App closed"
-            break
-        except Exception as ex:
-            print("Caught exception! Terminating..")
+    run("localhost", 1883, ["wx", "geo", "buttons"])
